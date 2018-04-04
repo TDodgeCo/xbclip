68b3509aae28f0c6edb0980e85fd4da92c6b8c7b
Tim Dodge <timldodge@gmail.com>
2018-04-03 20:51:55 -0600

    added flag -n functionality

--- api/controllers/xclipController.js
+++ api/controllers/xclipController.js
@@ -6,24 +6,47 @@ exports.slack_response = function (req, res) {
 
   var dirtyText = req.body.text
   var response_url = req.body.response_url
-  var text = dirtyText.replace(' ', '%20')
+  // var text = dirtyText.replace(' ', '%20')
+
+  // find out if there was a flag on the request
+  var textArr = dirtyText.split('')
+  var len = textArr.length
+  var vidIndex,
+      gTag,
+      responseMessage
+
+  if (textArr[len -2] === '-') {
+    vidIndex = textArr[len - 1] - 1
+    for (let i = 3; i > 0; i--) {
+      textArr.pop()
+    }
+    gTag = textArr.join('').replace(' ', '%20')
+    responseMessage = ' Clip #' + (vidIndex + 1) + ' from your collection is coming up.';
+    console.log('gTag is ' + gTag + ' vidIndex is ' + vidIndex)
+  }
+  else {
+    gTag = dirtyText.replace(' ', '%20')
+    vidIndex = 0
+    responseMessage = ' Your latest clip is coming up.'
+  }
+  var gamertag = gTag.replace('%20', ' ')
 
   // tell slack that we've received the response, allowing us to send delayed responses
   res.json(200, {
     'response_type': 'in_channel',
-    'text': 'On it, ' +  dirtyText + '.  Your latest clip is coming up.'
+    'text': 'On it, ' +  gamertag + '.' + responseMessage
   })
 
   // get the gamertag's xuid from xboxapi
   axios({
     method: 'get',
-    url: 'https://xboxapi.com/v2/xuid/' + text,
+    url: 'https://xboxapi.com/v2/xuid/' + gTag,
     headers: {
       'X-AUTH': process.env.X_AUTH
     }
   }).then( response =>  {
     var xuid = response.data.xuid
-
+    console.log(xuid)
     // use the xuid to find the latest clip for that xuid
     axios({
       method: 'get',
@@ -32,10 +55,8 @@ exports.slack_response = function (req, res) {
         'X-AUTH': process.env.X_AUTH
       }
     }).then( response =>  {
-      var clip = response.data[0].gameClipUris[0].uri
-      var title = response.data[0].titleId
-      console.log('title is - ' + title)
-
+      var clip = response.data[vidIndex].gameClipUris[0].uri
+      console.log(clip)
       axios({
         method: 'post',
         url: 'https://www.googleapis.com/urlshortener/v1/url?key=' + process.env.GOOG_API,
@@ -51,45 +72,14 @@ exports.slack_response = function (req, res) {
         // shorten URL and post that video clip to slack
         axios.post(response_url, {
           response_type: 'in_channel',
-          text: dirtyText + ' shared their latest clip with the channel - \n' + shortUrl + '\n This video will expire in 60 minutes.'
+          text: gamertag + ' shared their latest clip with the channel - \n' + shortUrl + '\n This video will expire in 60 minutes.'
         }).then(function(response) {
           console.log('clip successfully sent')
         })
       })
-
-      // Check if the game is PUBG
-      // if (title === 950328474) {
-      //   console.log('title is pubg - should return stats')
-      //   axios({
-      //     method: 'get',
-      //     url: 'https://xboxapi.com/v2/' + xuid + '/game-stats/' + 950328474,
-      //     headers: {
-      //       'X-AUTH': process.env.X_AUTH
-      //     }
-      //   }).then(function (response) {
-      //     var data = response.data.groups[0].statlistscollection[0].stats
-      //     var matches = data[0].value
-      //     var wins = data[1].value
-      //     var kills = data[2].value
-      //     var headshots = data[3].value
-      //     var kpm = kills / matches
-      //     var wpm = wins / matches
-      //     var hsr = headshots / kills
-      //
-      //     axios.post(response_url, {
-      //       response_type: 'in_channel',
-      //       text: 'Looks like the clip was from PUBG - Here are some stats for ' + dirtyText + '. \n'
-      //       + 'Matches Played: ' + matches + ' Chicken Dinners: ' + wins + ' Total Kills: ' + kills + ' Total Headshots: ' + headshots + '\n'
-      //       + 'Kills Per Match: ' + kpm + ' Win/Match Ratio: ' + wpm + ' Headshot/Kill Ratio: ' + hsr
-      //     }).then(function(response) {
-      //       console.log('stats successfully sent')
-      //     })
-      //   })
-      // }
-
     })
   })
-  console.log('Slash Command Text: ' + text + '\n' + 'Response URL: ' + response_url)
+  console.log('Slash Command Text: ' + gamertag + '\n' + 'Response URL: ' + response_url)
 }
 
 exports.get_response = function (req, res) {
--- toy.js
+++ toy.js
@@ -1,16 +1,22 @@
-dirtyText = 'hello world -2'
-
-var textArr = dirtyText.split('')
-var len = textArr.length
-var vidIndex,
-    gTag
-
-if (textArr[len -2] === '-') {
-  vidIndex = textArr[len - 1]
-  for (i = 3; i > 0; i--) {
-    textArr.pop()
-  }
-  gTag = textArr.join('').replace(' ', '%20')
-  console.log(gTag + ' ' + vidIndex)
-}
+// dirtyText = 'hello world -2'
+//
+// var textArr = dirtyText.split('')
+// var len = textArr.length
+// var vidIndex,
+//     gTag
+//
+// if (textArr[len -2] === '-') {
+//   vidIndex = textArr[len - 1]
+//   for (i = 3; i > 0; i--) {
+//     textArr.pop()
+//   }
+//   gTag = textArr.join('').replace(' ', '%20')
+//   console.log(gTag + ' ' + vidIndex)
+// }
 // else console.log(textArr)
+
+
+var link1 = ''
+var link2 = ''
+
+if (link1 === link2) {return true}
